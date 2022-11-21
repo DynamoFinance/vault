06951c51b70cc7c3495d2a970989242342640f7c
Benjamin Scherrey <scherrey@proteus-tech.com>
2022-11-21 18:41:32 +0700

    Trying to get pytest to smartly skip tests when wrong environment is active.

--- tests/test_Fork.py
+++ tests/test_Fork.py
@@ -1,6 +1,21 @@
-import ape
-
 import pytest
 
+import ape
+from ape import chain
+
+
+
+def is_not_hard_hat():
+    try:
+        print("Current chain id is: %s." % chain.chain_id)
+        return chain.chain_id!=31337
+    except ape.exceptions.ProviderNotConnectedError:
+        print("Alert: Not connected to a chain.")
+        return True
+
+@pytest.mark.skipif(is_not_hard_hat(), reason="Only run when connected to hard hat.")
 def test_vitalik_balance():
     assert ape.api.address.Address("0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045").balance == 739992029493125111147
+
+def test_always_good():
+    assert True
