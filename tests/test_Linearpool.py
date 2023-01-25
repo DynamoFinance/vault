a9bf0ad26549006697db3cc62436599829849364
Sajal Kayan <sajal83@gmail.com>
2023-01-25 20:17:05 +0700

    play with ComposableStablePool

--- tests/test_Linearpool.py
+++ tests/test_Linearpool.py
@@ -6,10 +6,6 @@ from web3 import Web3
 
 
 #ETH Mainnet addrs
-WETH  = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-BAL   = "0xba100000625a3754423978a60c9317c58a424e3D"
-POOL_BAL_WETH = "0x5c6Ee304399DBdB9C8Ef030aB642B10820DB8F56"
-pool_BAL_WETH = "0x5c6ee304399dbdb9c8ef030ab642b10820db8f56000200000000000000000014"
 VAULT = "0xBA12222222228d8Ba445958a75a0704d566BF2C8"
 
 
@@ -21,14 +17,6 @@ def deployer(accounts):
 @pytest.fixture
 def trader(accounts):
     return accounts[1]
-
-@pytest.fixture
-def weth(project):
-    return project.IERC20.at(WETH)
-
-@pytest.fixture
-def bal(project):
-    return project.IERC20.at(BAL)
 
 @pytest.fixture
 def vault(project):
@@ -68,8 +56,8 @@ def linear_pool(project, deployer, dai, ddai4626):
         VAULT,
         "DAI 4626 linear pool",
         "dDAI",
-        dai.address, #mainToken
-        ddai4626.address, #wrappedToken
+        dai, #mainToken
+        ddai4626, #wrappedToken
         2100000000000000000000000, #upperTarget = 2100000.0 DAI (by default lower target is 0, can be raised after enough liquidity is present)
         10000000000000, #swapFeePercentage = 0.001% (10000000000000 = 10^13 ; 10^18/10^13 = 100000; 100 / 100000 = 0.001%)
         7332168, #pauseWindowDuration
