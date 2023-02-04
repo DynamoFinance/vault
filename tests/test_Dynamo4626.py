771a4683ed8f6756a5f37a20edfff06175d89298
Ben Scherrey <scherrey@proteus-tech.com>
2023-02-05 01:24:58 +0700

    Now confirm event generation for add_pool().

--- tests/test_Dynamo4626.py
+++ tests/test_Dynamo4626.py
@@ -2,6 +2,8 @@ import pytest
 import ape
 from tests.conftest import is_not_hard_hat
 
+from itertools import zip_longest
+
 d4626_name = "DynamoDAI"
 d4626_token = "dyDAI"
 d4626_decimals = 18
@@ -34,6 +36,19 @@ def dynamo4626(project, deployer, dai, trader):
     return v
 
 
+# result is an ape.Result
+# event_names is an in-order list of strings of the names of the events generated in the tx.
+# if full_match == True, event_names must match all result events.
+# if full_match == False, we only check as many events as the event_names list has and ignore 
+#                         extra event logs that may exist.
+def events_in_logs(result, event_names, full_match=True) -> bool:
+    for a,b in zip_longest(result.decode_logs(), event_names):
+        if b == None and full_match == False: continue
+        assert a.event_name == b
+    return True    
+
+
+
 def test_basic_initialization(project, deployer, dynamo4626):
     assert dynamo4626.name(sender=deployer) == d4626_name
     assert dynamo4626.symbol(sender=deployer) == d4626_token
@@ -54,6 +69,7 @@ def test_add_pool(project, deployer, dynamo4626, pool_adapter, trader, dai):
     # pool_adapter is valid & deployer is allowed to add it.
     result = dynamo4626.add_pool(pool_adapter, sender=deployer) 
     assert result.return_value == True
+    assert events_in_logs(result, ["poolAdded"])
 
     # can't add it a second time.
     with ape.reverts("pool already supported."):
@@ -71,6 +87,7 @@ def test_add_pool(project, deployer, dynamo4626, pool_adapter, trader, dai):
         a = deployer.deploy(project.MockLPAdapter)
         result = dynamo4626.add_pool(a, sender=deployer) 
         assert result.return_value == True
+        assert events_in_logs(result, ["poolAdded"])
 
     # One more pool is too many however.
     a = deployer.deploy(project.MockLPAdapter)
