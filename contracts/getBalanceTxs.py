c5ac5457d28f5809753f43a28db8eaffb031132a
Ben Scherrey <scherrey@proteus-tech.com>
2023-02-13 02:11:36 +0700

    Initial python implementation of getBalanceTxs.

--- contracts/getBalanceTxs.py
+++ contracts/getBalanceTxs.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 MAX_POOLS = 5
 
 
-@dataclass
+#@dataclass
 class PoolAdapter:
     pass
 
@@ -13,13 +13,19 @@ class BalanceTX:
     Qty: int
     Adapter: PoolAdapter
 
+    def str(self) -> str:
+        return "Qty:%s, Adapter:%s" % (self.Qty, self.Adapter.self)
+
 
 @dataclass
 class ERC20:
-    balanceOf: dict
+    _balanceOf: dict
     def deposit(self, addr, amt):
-        val = self.balanceOf.get(addr,0)
-        self.balanceOf[addr] = val+amt
+        val = self._balanceOf.get(addr,0)
+        self._balanceOf[addr] = val+amt
+
+    def balanceOf(self, addr) -> int:
+        return self._balanceOf.get(addr,0)
 
 
 
@@ -27,6 +33,7 @@ class ERC20:
 class Pool:
     dlending_pools : list[PoolAdapter] 
     derc20asset : ERC20
+    strategy : list[int]
 
     def __eq__(self, other) -> bool:
         return self == other.self
@@ -39,35 +46,143 @@ class Pool:
 
         # result : DynArray[BalanceTX, MAX_POOLS] = empty(DynArray[BalanceTX, MAX_POOLS])
         # result : BalanceTX[MAX_POOLS] = list[BalanceTX]
-        result : list[BalanceTX] = [BalanceTX for x in range(MAX_POOLS)]
+        result : list[BalanceTX] = [BalanceTX(Qty=0,Adapter=None) for x in range(_max_txs)]
 
         # If there are no pools then nothing to do.
         if len(self.dlending_pools) == 0: return result
 
-        current_local_asset_balance : int = self.derc20asset.balanceOf.get(self, 0)
+        total_balance = self.derc20asset.balanceOf(self) + sum([self.derc20asset.balanceOf(pool) for pool in self.dlending_pools])
+        available_balance = total_balance - _target_asset_balance
+        total_shares = sum(self.strategy)
+        targetBalances = [0 for x in range(MAX_POOLS)]
+        currentBalances = [0 for x in range(MAX_POOLS)]
+        deltaBalances = [0 for x in range(MAX_POOLS)]
+        sum_in = 0 
+        sum_out = 0
+        for pos, pool in enumerate(self.dlending_pools):
+            targetBalances[pos] = int((self.strategy[pos]/total_shares) * available_balance)
+            currentBalances[pos] = int(self.derc20asset.balanceOf(pool))
+            deltaBalances[pos] = int(targetBalances[pos] - currentBalances[pos])
+            if deltaBalances[pos] > 0:
+                sum_out += deltaBalances[pos]
+            else:
+                sum_in += deltaBalances[pos]
 
-        # TODO - Just going to assume one adapter for now.
-        pool : PoolAdapter = self.dlending_pools[0]
-        delta_tx: int = current_local_asset_balance - _target_asset_balance
-        dtx: BalanceTX = BalanceTX(Qty= delta_tx, Adapter= pool)
+        leftover_balance = available_balance - (sum(currentBalances) + sum(deltaBalances))
+        deltaTarget = self.derc20asset.balanceOf(self) - _target_asset_balance 
 
-        # result.append(dtx)
-        result[0] = dtx
+        print("\nPool Adapters: %s." % self.dlending_pools)
+        print("total_balance = %s." % total_balance)            
+        print("targetBalances = %s." % targetBalances)
+        print("currentBalances = %s." % currentBalances)
+        print("deltaBalances = %s." % deltaBalances)
+        print("leftover_balance = %s." % leftover_balance)
+        print("_target_asset_balance = %s." % _target_asset_balance)
+        print("deltaTarget = %s." % deltaTarget)
+        print("sum_in = %s." % sum_in)
+        print("sum_out = %s." % sum_out)
+
+        tx : int = 0
+        for i in range(len(self.dlending_pools)):
+            if deltaTarget < 0:
+                lowest : int = 0
+                lowest_pos : int = 0
+                for pos in range(len(self.dlending_pools)):
+                    # Need to bring funds into the pool now.
+                    if deltaBalances[pos] < lowest:
+                        lowest = deltaBalances[pos]
+                        lowest_pos = pos
+                result[tx] = BalanceTX(Qty = lowest, Adapter=self.dlending_pools[lowest_pos])
+                deltaBalances[lowest_pos] = 0
+                deltaTarget -= lowest
+                tx+=1
+                continue
+            else:
+                largest : int = 0
+                largest_pos : int = 0                
+                for pos in range(len(self.dlending_pools)):
+                    # Now try for the largest abs(tx).
+                    if abs(deltaBalances[pos]) > abs(largest):
+                        # Make sure we don't pull below our target.
+                        if deltaTarget + deltaBalances[pos] < 0: continue
+                        largest = deltaBalances[pos]
+                        largest_pos = pos
+                result[tx] = BalanceTX(Qty = largest, Adapter=self.dlending_pools[largest_pos])
+                deltaBalances[largest_pos] = 0
+                deltaTarget += largest
+                tx+=1
+                continue
+
+        # This likely never can be called.
+        # for i in range(len(self.dlending_pools)):
+        #     for pos in range(len(self.dlending_pools)):
+        #         if deltaBalances[pos] != 0:
+        #             result[tx] = BalanceTX(Qty = deltaBalances[pos], Adapter=self.dlending_pools[pos])
+        #             tx+=1
+
+        # Make sure we meet our _target_asset_balance goal within _max_txs steps!
+        running_balance : int = self.derc20asset.balanceOf(self)
+        for pos in range(_max_txs):
+            running_balance -= result[pos].Qty
+
+        if running_balance < _target_asset_balance:
+            print("Running short! Adjust!")
+            diff : int = _target_asset_balance - running_balance
+            for pos in range(_max_txs):
+                available_funds = self.derc20asset.balanceOf(result[pos].Adapter) + result[pos].Qty
+                if available_funds >= diff:
+                    result[pos].Qty-=diff
+                    diff=0
+                    break
+                elif available_funds > 0:
+                    result[pos].Qty-=available_funds
+                    diff+=available_funds
+
+            assert diff <= 0, "CAN'T BALANCE IN %s STEP!" % _max_txs
+
+        if len(result) > _max_txs:
+            for pos in range(len(result)-_max_txs):
+                result[pos+_max_txs]=BalanceTX(Qty=0,Adapter=PoolAdapter())
+
+        # DEBUGGING ONLY
+        running_balance : int = self.derc20asset.balanceOf(self)                
+        for tx in result:
+            if tx.Adapter == None: break
+            running_balance -= tx.Qty
+            adapt_pos = self.dlending_pools.index(tx.Adapter)
+            print("Adapter:%s target balance: %s, final balance: %s." % (tx.Adapter,targetBalances[adapt_pos],currentBalances[adapt_pos]+tx.Qty))
+        print("Pool target balance: %s, final balance: %s." % (_target_asset_balance,running_balance))            
 
         return result
 
 
 d = {}
-dai = ERC20(balanceOf = d)
+dai = ERC20(_balanceOf = d)
 
-adapters = [PoolAdapter()]
+a1 = PoolAdapter()
+a2 = PoolAdapter()
 
-p = Pool(adapters, dai)
+adapters = [a1,a2]
 
+p = Pool(adapters, dai, [0 for x in range(MAX_POOLS)])
+p.strategy[0] = 50
+p.strategy[1] = 50
+
+dai.deposit(a1, 20)
 
 dai.deposit(p,500) 
 
 
 result = p.getBalanceTxs(0, 5)
+
+print(result)
+
+result = p.getBalanceTxs(250, 5)
+
+print(result)
+
+dai.deposit(a2, 600)
+
+result = p.getBalanceTxs(545, 5)
 
 print(result)
